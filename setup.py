5f864261369b3ffa95d53e51d4bfbb9702727156
Malcolm Ramsay <malramsay64@gmail.com>
2017-10-25 14:18:11 +1100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+""" """
+
+from distutils.core import setup
+
+setup(
+  name='PhD',
+  packages=['PhD'], # this must be the same as the name above
+  version='0.1',
+  description='A random test lib',
+  author='Malcolm Ramsay',
+  author_email='malramsay64@gmail.com',
+  url='https://github.com/malramsay64/pip-phd', # use the URL to the github repo
+  download_url='https://github.com/malramsay64/php-phd/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords=['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers=[],
+)
